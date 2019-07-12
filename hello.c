fa3f869ac1a708390972887883d912b16479ac53
Andrew Huff <ahuff@instructure.com>
2019-07-12 15:24:37 -0600

    tutorial 2

--- hello.c
+++ hello.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <SDL2/SDL.h>
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-int main(int argc, char** argv) {
-  printf("Starting\n");
-  SDL_Window* window = NULL;
-  SDL_Surface* screenSurface = NULL;
+SDL_Window* window = NULL;
+SDL_Surface* screenSurface = NULL;
+
+bool init() {
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     printf("SDL failed to initialize: SDL_Error %s\n", SDL_GetError());
-    return 1;
+    return false;
   }
 
   window = SDL_CreateWindow("SDL Tutorial",
@@ -19,17 +20,49 @@ int main(int argc, char** argv) {
     SDL_WINDOW_SHOWN);
   if (window == NULL) {
     printf("Could not create window: SDL_Error %s\n", SDL_GetError());
-    return 1;
+    return false;
   }
 
   screenSurface = SDL_GetWindowSurface(window);
-  SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+  return true;
+}
+
+SDL_Surface* helloSurface = NULL;
+bool loadMedia() {
+  helloSurface = SDL_LoadBMP("hello_world.bmp");
+  if (helloSurface == NULL) {
+    printf("Could not load image %s: SDL_Error: %s\n", "hello_world.bmp", SDL_GetError());
+    return false;
+  }
+  return true;
+}
+
+void close() {
+  SDL_DestroyWindow(window);
+  window = NULL;
+
+  SDL_Quit();
+}
+
+int main(int argc, char** argv) {
+  bool success;
+  success = init();
+  if (!success) {
+    printf("Failed to init\n");
+    return 1;
+  }
+
+  success = loadMedia();
+  if (!success) {
+    printf("Failed to load media\n");
+    return 1;
+  }
+
+  SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0x00, 0xFF));
+  SDL_BlitSurface(helloSurface, NULL, screenSurface, NULL);
   SDL_UpdateWindowSurface(window);
 
-  SDL_Delay(2000);
+  SDL_Delay(3000);
 
-  // redundant cleanup nonsense:
-  SDL_DestroyWindow(window);
-  SDL_Quit();
-  return 0;
+  close();
 }
