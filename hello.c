cf90c540126e7c9f11f50afccb6449ed97277ca0
Andrew Huff <ahuff@instructure.com>
2019-07-12 14:48:24 -0600

    sdl hello world
    Following this tutorial:
    http://lazyfoo.net/tutorials/SDL/index.php

--- hello.c
+++ hello.c
@@ -2,6 +2,34 @@
 #include <stdlib.h>
 #include <SDL2/SDL.h>
 
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
 int main(int argc, char** argv) {
-  printf("Hello world 4\n");
+  printf("Starting\n");
+  SDL_Window* window = NULL;
+  SDL_Surface* screenSurface = NULL;
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    printf("SDL failed to initialize: SDL_Error %s\n", SDL_GetError());
+    return 1;
+  }
+
+  window = SDL_CreateWindow("SDL Tutorial",
+    SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT,
+    SDL_WINDOW_SHOWN);
+  if (window == NULL) {
+    printf("Could not create window: SDL_Error %s\n", SDL_GetError());
+    return 1;
+  }
+
+  screenSurface = SDL_GetWindowSurface(window);
+  SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+  SDL_UpdateWindowSurface(window);
+
+  SDL_Delay(2000);
+
+  // redundant cleanup nonsense:
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+  return 0;
 }
